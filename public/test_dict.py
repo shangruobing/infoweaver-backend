6eade2f711172b5408f2d3ad048b5f8ebe89b77b
Ruobing Shang <shangruobing29@163.com>
2022-04-19 10:22:33 +0800

    refactor project construction

--- public/test_dict.py
+++
@@ -1,9 +0,0 @@
-import jieba
-import jieba.posseg as pesg
-
-string = '什么时候举办四六级'
-# jieba.enable_paddle()
-jieba.load_userdict('dictionary.txt')
-words = pesg.cut(string, use_paddle=True)
-for i in words:
-    print(i.word, i.flag)
